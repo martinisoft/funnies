873bb3bdf4204d5e28097feff1a7106d2e73dba1
Joshua Davey and Aaron Kalin <dev+jgdavey+martinisoft@hashrocket.com>
2011-01-28 16:03:03 -0600

    Add standard factory and session steps

---
+++ features/step_definitions/session_steps.rb
@@ -0,0 +1,32 @@
+Given /^I am signed in as the following (\w+):$/ do |role, table|
+  Given %(the following #{role}:), table
+  @me = @it
+  Given 'I am signed in'
+end
+
+Given /^I am logged in as the following user:$/ do |table|
+  Given "the following user:", table
+  When %Q{I sign in as "#{@user.email}"}
+end
+
+Given /^I (?:am signed|sign) in as an? (\w+)$/ do |role|
+  @me = Factory(role.to_sym)
+  Given 'I am signed in'
+end
+
+
+Given 'I am signed in' do
+  @me ||= Factory(:user)
+  When %(I go to the new user session page)
+  When %(I fill in "Login" with "#{@me.email}")
+  When %(I fill in "Password" with "#{@me.password}")
+  When %(I press "Sign in")
+end
+
+
+When /^I sign in as "([^"]*)"$/ do |email|
+  @me = User.find_by_email(email)
+  @me.password ||= 'password'
+  Given 'I am signed in'
+end
+
