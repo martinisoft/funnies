0f21414d9c6ad9cfa43ee68e3da08dcd4cea3a41
Aaron Kalin <akalin@martinisoftware.com>
2011-02-03 12:36:53 -0600

    Trim out devise options comment

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,4 @@
 class User < ActiveRecord::Base
-  # Include default devise modules. Others available are:
-  # :token_authenticatable, :confirmable, :lockable and :timeoutable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable,
          :token_authenticateable, :confirmable, :lockable
