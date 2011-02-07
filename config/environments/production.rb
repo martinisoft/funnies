2710f5d5fecde6d7d9ef6f3660d0ac43d66770d0
Aaron Kalin <akalin@martinisoftware.com>
2011-02-07 10:33:19 -0600

    Change default host to heroku app domain
    - This was causing a production issue on Heroku where it was not being
      set correctly and caused the app to crash.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -46,4 +46,7 @@ Funnies::Application.configure do
 
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
+
+  # Change default host URL
+  config.action_mailer.default_url_options = { :host => "funnies.heroku.com" }
 end
