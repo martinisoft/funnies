f86fdcaa6b9fd6064a1182113ca973dcc2feb137
Aaron Kalin <akalin@martinisoftware.com>
2011-03-07 01:04:29 -0600

    Remove peppers
    This is to enhance the security of the dpeloyed app, you'll want to
    un-comment these in your deployment.
    
    May I recommend my excellent seasoning gem for generating new peppers?

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Funnies::Application.config.secret_token = '50226643472e5e615c49e3a56300875457b25b863cd7d4b0d9109bec2ce66f7f825cddab83941c027610c17c7ba1fd01ba154086eff9c1ff46624f415867ca0e'
+# Funnies::Application.config.secret_token = '50226643472e5e615c49e3a56300875457b25b863cd7d4b0d9109bec2ce66f7f825cddab83941c027610c17c7ba1fd01ba154086eff9c1ff46624f415867ca0e'
