2746bc87e05e0f1810119b7a3132718f305a48e3
Aaron Kalin <akalin@martinisoftware.com>
2011-03-10 15:02:20 -0600

    Require responders in the Application

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,8 @@
+require "application_responder"
+
 class ApplicationController < ActionController::Base
+  self.responder = ApplicationResponder
+
   protect_from_forgery
 
   layout :layout_by_resource
