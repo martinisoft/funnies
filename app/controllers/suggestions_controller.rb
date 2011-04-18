7542200a6f33dd2ed87a2dca723699674c4d4f74
Aaron Kalin <akalin@martinisoftware.com>
2011-04-18 01:50:41 -0400

    Ensure user is saved with the suggestion

--- app/controllers/suggestions_controller.rb
+++ app/controllers/suggestions_controller.rb
@@ -5,6 +5,7 @@ class SuggestionsController < ApplicationController
   before_filter :authenticated
 
   def create
+    suggestion.user = current_user
     suggestion.save
     respond_with(suggestion, location: comics_path)
   end
