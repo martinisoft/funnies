ee2fed7ef2dd21f200bad22889b43e352fdb3fbd
Aaron Kalin <akalin@martinisoftware.com>
2012-01-10 08:08:35 -0600

    Add email-spec

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,6 +4,7 @@ require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
 require 'rspec/autorun'
 require 'turnip/capybara'
+require 'email_spec'
 
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
@@ -31,4 +32,8 @@ RSpec.configure do |config|
   # automatically. This will be the default behavior in future versions of
   # rspec-rails.
   config.infer_base_class_for_anonymous_controllers = false
+
+  # Include EmailSpec Helpers
+  config.include EmailSpec::Helpers
+  config.include EmailSpec::Matchers
 end
