f01c025d323a3eecb1eb8347bae76861814b8f4f
Aaron Kalin <akalin@martinisoftware.com>
2012-01-12 22:26:50 -0600

    Re-factor: User accounts integration test

--- spec/fabricators/user_fabricator.rb
+++ spec/fabricators/user_fabricator.rb
@@ -3,8 +3,11 @@ Fabricator(:user) do
   username { Fabricate.sequence(:username) { |i| "martinisoft-#{i}" } }
   email { Fabricate.sequence(:email) { |i| "user+#{i}@example.com" } }
   password "foobar"
-  password_confirmation { |f| f.password }
-  confirmed_at 2.hours.ago
+  password_confirmation { |user| user.password }
+end
+
+Fabricator :confirmed_user, from: :user do
+  after_create { |user| user.confirm! }
 end
 
 Fabricator(:admin, from: :user) do
