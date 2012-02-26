c5bd05a8be491d90e27341ba1f36f156d6425fb5
Aaron Kalin <akalin@martinisoftware.com>
2012-02-25 19:59:50 -0600

    Reader unsubscribes from comic

--- spec/features/reader/reader_subscriptions.feature
+++ spec/features/reader/reader_subscriptions.feature
@@ -8,3 +8,11 @@ Feature: Reader subscriptions
     And there is a comic to subscribe to
     When I subscribe to that comic
     Then I see that comic in my personal feed
+
+  Scenario: Reader unsubscribes
+    Given I am signed in as a reader
+    And there is a comic to subscribe to
+    And I subscribe to that comic
+    When I unsubscribe from that comic
+    Then I don't see that comic in my personal feed
+    And I can subscribe to that comic again
--- spec/features/reader/reader_subscriptions_steps.rb
+++ spec/features/reader/reader_subscriptions_steps.rb
@@ -9,14 +9,36 @@ steps_for :reader_subscriptions do
     @comic = Fabricate :comic
   end
 
-  step "I subscribe to that comic" do
+  step "I :subscribe to/from that comic" do |action|
     visit comics_path
-    click_link "Subscribe"
+    click_link action.titleize
   end
 
   step "I see that comic in my personal feed" do
+    visit user_comics_path(@user)
     within "article.comic" do
       page.should have_content @comic.name
+    end
+  end
+
+  step "I don't see that comic in my personal feed" do
+    visit user_comics_path(@user)
+    page.should_not have_css "article.comic"
+  end
+
+  step "I can subscribe to that comic again" do
+    visit comics_path
+    within "article.comic" do
+      page.should have_content @comic.name
+    end
+    within ".comic_nav" do
+      page.should have_link "Subscribe"
+    end
+  end
+
+  placeholder :subscribe do
+    match /(subscribe|unsubscribe)/ do |action|
+      action
     end
   end
 end
