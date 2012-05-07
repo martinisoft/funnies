ec3b783e4c2eb1760cf05ce6698f9e84e6f23fbd
Aaron Kalin <akalin@martinisoftware.com>
2012-05-06 21:01:17 -0500

    Cleanup

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,7 +10,7 @@ class ApplicationController < ActionController::Base
   def after_sign_in_path_for(resource_or_scope)
     case resource_or_scope
     when :user, User
-      user_comics_path(:username => current_user.username)
+      user_comics_path(username: current_user.username)
     else
       super
     end
