e6f551a547c39aea43854bfd467d67f04261d695
Aaron Kalin <akalin@martinisoftware.com>
2012-10-21 11:46:06 -0700

    Include devise helpers for controller specs

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -37,6 +37,9 @@ RSpec.configure do |config|
   config.include EmailSpec::Helpers
   config.include EmailSpec::Matchers
 
+  # Include devise helpers
+  config.include Devise::TestHelpers, type: :controller
+
   # Run specs in random order to surface order dependencies. If you find an
   # order dependency and want to debug it, you can fix the order by providing
   # the seed, which is printed after each run.
