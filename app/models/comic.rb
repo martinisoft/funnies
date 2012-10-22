0ab06757cbb4075d3e9abe36b5f469b04f16c61d
Aaron Kalin <akalin@martinisoftware.com>
2012-10-22 14:38:53 -0700

    Re-factor comic scraping methods

--- app/models/comic.rb
+++ app/models/comic.rb
@@ -14,26 +14,26 @@ class Comic < ActiveRecord::Base
   attr_accessible :name, :homepage, :comic_page, :xpath_title, :xpath_image
 
   def update_strip
-    if source_image_url.present?
-      hash = Digest::MD5.hexdigest(open(source_image_url).read)
-      unless ComicStrip.find_by_md5_hash(hash)
-        comic_strips.create(:remote_comic_image_url => source_image_url)
-      end
+    return nil unless source_image_url.present?
+
+    hash = Digest::MD5.hexdigest(open(source_image_url).read)
+    unless ComicStrip.find_by_md5_hash(hash)
+      comic_strips.create(remote_comic_image_url: source_image_url)
     end
   end
 
   def source_image_url
-    begin
-      doc = Nokogiri::HTML(open(self.comic_page))
-      image_url = doc.xpath("#{xpath_image}/@src").to_s
-      return unless image_url.present?
-      unless image_url.match(/^http:\/\//)
-        image_url = "#{self.homepage}/#{image_url}"
-      end
-      image_url
-    rescue Exception => e
-      logger.info "failed to parse image:", e
-      nil
+    doc = Nokogiri::HTML(open(self.comic_page))
+    image_url = doc.xpath("#{xpath_image}/@src").to_s
+
+    return unless image_url.present?
+    unless image_url.match(/^http:\/\//)
+      image_url = "#{self.homepage}/#{image_url}"
     end
+
+    image_url
+  rescue => e
+    logger.info "failed to parse image:", e
+    nil
   end
 end
