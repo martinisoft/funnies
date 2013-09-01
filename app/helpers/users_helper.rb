b67057bab217d253d994fe4332610fb48c8b3ed0
Aaron Kalin <akalin@martinisoftware.com>
2013-09-01 12:01:54 -0500

    Don't allow the secure option to be overridden

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,5 +1,7 @@
 module UsersHelper
-  def gravatar_for(user, options = { size: 50, secure: true })
+  def gravatar_for(user, options = { size: 50 })
+    # Always secure URLs
+    options = options.merge({ secure: true })
     gravatar_image_tag(user.email.downcase, alt: user.username,
                                             class: 'gravatar',
                                             gravatar: options)
