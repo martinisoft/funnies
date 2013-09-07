9bfd54190cb60b8bf0feb4c24e06b38572a2d11a
Aaron Kalin <akalin@martinisoftware.com>
2013-09-07 03:27:38 -0500

    Add more keys for devise and rails

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -10,4 +10,4 @@
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
 Funnies::Application.config.secret_token = ENV['SECRET_TOKEN']
-Funnies::Application.config.secret_key_base = ENV['SECRET_TOKEN']
+Funnies::Application.config.secret_key_base = ENV['SECRET_KEY_BASE']
