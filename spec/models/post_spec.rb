58f5ecd746c40f55203d09588daab1431d88e037
Aaron Kalin <akalin@martinisoftware.com>
2013-09-23 02:49:40 -0500

    Remove invalid assertion

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -8,6 +8,5 @@ describe Post do
   describe "validations" do
     it { should validate_presence_of(:title) }
     it { should validate_presence_of(:body) }
-    it { should validate_presence_of(:user_id) }
   end
 end
